82317094e2469e1ae25c6f154ae7f0c00283216b
jvrsgsty <jvrsgsty@gmail.com>
2017-01-18 19:56:20 +0000

    Agrega el backend de autos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,35 @@
 class HomeController < ApplicationController
   def index
   end
+  
+  def marcas_modelos
+    autos = '{"marcas" : [
+      {"marca" : {"nombre": "Audi", "modelos": [
+          {"modelo": {"nombre" : "A1"}},
+          {"modelo": {"nombre" : "Q5"}},
+          {"modelo": {"nombre" : "R8"}}
+        ]}},
+      {"marca" : {"nombre": "BMW", "modelos": [
+          {"modelo": {"nombre" : "350"}},
+          {"modelo": {"nombre" : "i8"}}
+        ]}},
+      {"marca" : {"nombre": "KIA", "modelos": [
+          {"modelo": {"nombre" : "Forte"}},
+          {"modelo": {"nombre" : "Rio"}},
+          {"modelo": {"nombre" : "Sportage"}}
+        ]}},
+      {"marca" : {"nombre": "Mazda", "modelos": [
+          {"modelo": {"nombre" : "3"}},
+          {"modelo": {"nombre" : "6"}},
+          {"modelo": {"nombre" : "MX-5"}}
+        ]}},
+      {"marca" : {"nombre": "Mercedes Benz", "modelos": [
+        {"modelo": {"nombre" : "C280"}},
+        {"modelo": {"nombre" : "S500"}},
+        {"modelo": {"nombre" : "B180"}}
+      ]}}
+    ]}'
+    
+    render json: autos
+  end
 end
